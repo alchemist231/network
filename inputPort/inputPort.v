7ce21c49ecb35f6e9ca0d42ddc41376fab7dbf2e
alchemist231 <abhinav23194@gmail.com>
2015-09-16 00:06:47 +0530

    generic input port added

--- inputPort/inputPort.v
+++ inputPort/inputPort.v
@@ -8,6 +8,9 @@ module inputPort(flit, routerAddress, portBlock, outFlitPort1, outFlitPort2, out
 	parameter flitWidth = ;
 	parameter modifiedFlitSize = ;
 	parameter router_add_size  = ;
+	parameter headerWidth      = ;
+	parameter modifiedHeaderWidth = ;
+	parameter messageSize =			;
  
 	input wire [flitWidth-1:0] flit;
 	input wire [router_add_size-1:0] routerAddress;
@@ -15,11 +18,21 @@ module inputPort(flit, routerAddress, portBlock, outFlitPort1, outFlitPort2, out
 	output wire [modifiedFlitSize-1:0] outFlitPort1, outFlitPort2, outFlitPort3, outputFlit4;
 
 	wire [modifiedFlitSize-1:0] modifiedFlit;
+	wire [headerWidth-1:0] header;
+	wire [modifiedHeaderWidth-1:0] modifiedHeader;
+	wire [1:0] route;
+	wire [messageSize-1:0] message;
 	reg portAddress
 
-	x_y_routing route(flit, routerAddress, portAddress, modifiedFlit);
+	assign header = flit[flitWidth-1:flitWidth-headerWidth+1];
+	assign message = flit[flitWidth-headerWidth:0];
 
-	demultiplexerFlit_1x4 demux(select, modifiedFlit, outFlitPort1, outFlitPort2, outFlitPort3, outFlitPort4);
+	x_y_routing route(header, routerAddress, portAddress, modifiedHeader);
 
+	assign modifiedFlit={modifiedHeader,message};
+
+	demultiplexerFlit_1x4 demux(modifiedHeader[modifiedHeaderWidth-1:modifiedHeaderWidth-2], modifiedFlit, outFlitPort1, outFlitPort2, outFlitPort3, outFlitPort4);
+
+endmodule
 
 
